961e8322864cd979fcf6d1e09e0288eb60928bab
Daniil Shmoylov <dshmoylov@icerockdev.com>
2025-05-03 13:10:17 +0300

    Initial Commit

---
+++ PhytoScope/PhytoScopeApp.swift
@@ -0,0 +1,32 @@
+//
+//  PhytoScopeApp.swift
+//  PhytoScope
+//
+//  Created by Daniil Shmoylov on 03.05.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PhytoScopeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
